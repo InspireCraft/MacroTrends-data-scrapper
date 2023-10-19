df12fb7e4b91b38890d2dd01fef990505f9172a0
AlpSari <50492914+AlpSari@users.noreply.github.com>
2023-10-19 21:53:41 +0200

    utils: Added Logger tests using unittest framework

--- src/utils/Logger.py
+++ src/utils/Logger.py
@@ -1,35 +1,36 @@
 import logging
 import colorlog
 
-logger_level_dict = {'none': logging.CRITICAL + 1,
-                     'critical': logging.CRITICAL,
-                     'info': logging.INFO,
-                     'debug': logging.DEBUG,
-                     'trace': logging.DEBUG - 1}
-
-colorlog_colors_dict = {
-    'DEBUG': 'reset',
-    'INFO': 'green',
-    'WARNING': 'yellow',
-    'ERROR': 'red',
-    'CRITICAL': 'red,bg_white',
-}
-
-colorlog_format = '%(log_color)s %(name)s: [%(levelname)s] - %(message)s'
-
 
 class Logger(logging.Logger):
     """Wrapper to the logging.Logger with an easier constructor."""
 
-    def __init__(self, name: str, logging_level_str: str = 'none'):
+    logger_level_dict = {
+        "none": logging.CRITICAL + 1,
+        "critical": logging.CRITICAL,
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
+        "trace": logging.DEBUG - 1,
+    }
 
+    colorlog_colors_dict = {
+        "DEBUG": "reset",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    }
+
+    colorlog_format = "%(log_color)s %(name)s: [%(levelname)s] - %(message)s"
+
+    def __init__(self, name: str, logging_level_str: str = "none"):
         logging_level_str = logging_level_str.lower()  # make it lowercase
 
         # Set the logging level
-        if logging_level_str in logger_level_dict.keys():
-            level = logger_level_dict[logging_level_str]
+        if logging_level_str in self.logger_level_dict.keys():
+            level = self.logger_level_dict[logging_level_str]
         else:
-            raise (KeyError('Check logging_level_str'))
+            raise (KeyError("Check logging_level_str"))
 
         # Create Logging object
         super().__init__(name, level)
@@ -38,9 +39,9 @@ class Logger(logging.Logger):
 
         # Formatter of console handler
         self.console_formatter = colorlog.ColoredFormatter(
-            colorlog_format,
-            log_colors=colorlog_colors_dict,
-            secondary_log_colors={}
+            self.colorlog_format,
+            log_colors=self.colorlog_colors_dict,
+            secondary_log_colors={},
         )
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(self.console_formatter)
@@ -52,14 +53,14 @@ class Logger(logging.Logger):
         if formatter is None:
             formatter = self.console_formatter
 
-        file_handler = logging.FileHandler(filename, encoding='utf-8')
+        file_handler = logging.FileHandler(filename, encoding="utf-8")
         file_handler.setFormatter(formatter)
 
-        self.info(f'Logging file is specified as: {filename}')
+        self.info(f"Logging file is specified as: {filename}")
         self.addHandler(file_handler)
 
 
 if __name__ == "__main__":
-    logger = Logger('LoggerFunctionality', 'debug')
+    logger = Logger("LoggerFunctionality", "debug")
     logger.info("Deneme -> Info")
     logger.debug("Deneme -> Debug")
