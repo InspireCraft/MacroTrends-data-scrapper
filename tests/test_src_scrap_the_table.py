fd3dc7956a49fe09c0560a56de30e5e12dbc430b
unknown <utku.tahan.ee@gmail.com>
2023-11-01 20:38:12 +0100

    resolve flake8 issues

--- tests/test_src_scrap_the_table.py
+++ tests/test_src_scrap_the_table.py
@@ -1,37 +1,58 @@
 import unittest
 from src.scrap_the_table import TableScrapper
 import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome import webdriver
 
 
 class TableScrapperTestCase(unittest.TestCase):
+    """Class to be used to test TableScrapper class.
+
+    Methods
+    -------
+    setUP():
+        setUp reused variables/objects
+
+    test_create_drivers():
+        check if the created driver object is WebDriver object
+
+    test_get_table_headers():
+        check if function return a dictionary
+        check if all the headers (corresponding to tabs) are scrapped or not
+
+    test_get_num_of_rows():
+        check if the function returns tuple of ints
+        check the returned numbers corresponds to some specific integers
+        check the magnitude relationship of the numbers
+    """
+
     def setUp(self):
+        """Set up reused variables/objects."""
         self.scrapper = TableScrapper()
         self.driver = self.scrapper._create_driver()
 
     def test_create_driver(self):
+        """Check if the created driver object is WebDriver object."""
         # Check if self.driver is a WebDriver object
         self.assertIsInstance(self.driver, selenium.webdriver.chrome.webdriver.WebDriver)
 
     def test_get_table_headers(self):
+        """Check if function return a dictionary.
+
+        Check if all the headers (corresponding to tabs) are scrapped or not
+
+        """
         self.header_list = self.scrapper._get_table_headers(self.driver)
         # Check if self.header_list is a dictionary
         self.assertIsInstance(self.header_list, dict)
 
         # Define what tabs have as headers
-        overview = ['Stock Name\n(Hover for 1Y Chart)',
-                     'Ticker',
-                     'Industry',
-                     'Market Cap',
-                     'Closing\nPrice',
-                     '1 Year\n% Change',
-                     'P/E Ratio',
-                     'Dividend\nYield']
+        overview = ['Stock Name\n(Hover for 1Y Chart)', 'Ticker', 'Industry', 'Market Cap',
+                    'Closing\nPrice', '1 Year\n% Change', 'P/E Ratio', 'Dividend\nYield']
 
         descriptive = ['Market Cap', 'Exchange', 'Country', 'Sector', 'Industry']
+
         dividend = ['Market Cap', 'Dividend\nYield', '12 Month\nDividend', '12 Month\nEPS',
                     'Dividend\nPayout Ratio']
+
         performance_st = ['1 Week\n% Change', '1 Month\n% Change', '3 Month\n% Change',
                           '6 Month\n% Change', 'YTD\n% Change', '1 Year\n% Change',
                           'Price vs\n50D SMA', 'Price vs\n200D SMA']
@@ -61,6 +82,12 @@ class TableScrapperTestCase(unittest.TestCase):
         self.assertEqual(self.header_list["rev_earnings"], rev_earnings)
 
     def test_get_num_of_rows(self):
+        """Check if the function returns tuple of ints.
+
+        Check the returned numbers corresponds to some specific integers
+        Check the magnitude relationship of the numbers
+
+        """
         (self.init, self.last, self.total) = self.scrapper._get_num_of_rows(self.driver)
         # Check if the row number types are integer
         self.assertIsInstance(self.init, int)
