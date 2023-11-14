fa1da980174f0f8d564ed254f485a4b844cb3f5c
unknown <utku.tahan.ee@gmail.com>
2023-11-14 22:19:49 +0100

    modify scrap the table based on the recent changes

--- src/scrap_the_table.py
+++ src/scrap_the_table.py
@@ -3,11 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from tqdm import tqdm
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 from src.utils.Logger import Logger
 import json
 from src.map_of_headers import map_of_headers
+from src.utils.create_driver import create_driver
 
 class TableScrapper:
     """
@@ -44,41 +43,14 @@ class TableScrapper:
         self.url = url
         self.logger = Logger(self.__class__.__name__, str_logger)
 
-        f_search = open("../src/config2.json")
+        # Read JSON file for parameters required to be searched
+        f_search = open("searchparameters.json")
         search_dict = json.load(f_search)
         f_search.close()
+
         self.search_params = [elem for elem in search_dict["search_parameters"]]
-
-        # f = open("../src/config.json")
-        # config = json.load(f)
-        # f.close()
-        # self.tab_names = [elem for elem in config["tab_names"].keys()]
-
         self.logger.info(f"Search Params = {self.search_params}...")
 
-    def _create_driver(self) -> "webdriver.chrome":
-        """Create driver object.
-
-        Driver is an WebDriver object that interacts with the website. Clicking,
-        reading are the methods of the driver object.
-
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        driver : WebDriver object
-
-        """
-        self.logger.info("WebDriver is being created...")
-        options = Options()
-        options.add_argument("--headless")  # Run selenium without opening an actual browser
-
-        driver = webdriver.Chrome(options=options)  # Initialize the driver instance
-        driver.get(self.url)
-        self.logger.info("WebDriver is created!!!")
-        return driver
 
     def _get_num_of_rows(self, driver) -> "tuple[int,int,int]":
         """Check current row number, max row number in current page, total row number.
@@ -110,16 +82,15 @@ class TableScrapper:
         company_attr_dict : dict
             dictionary of the companies associated with their properties
         """
-        driver = self._create_driver()
+        driver = create_driver(self.url, self.logger)
         self.logger.info("SCRAPPING STARTED...")
         ###
         # FILTERING CAN BE INTEGRATED HERE
         ###
         (init_num, final_num, max_num) = self._get_num_of_rows(driver)
         company_attr_dict = {}
-        CNT = 0
         with tqdm(total=max_num) as pbar:
-            while final_num != max_num and CNT<2:
+            while final_num != max_num:
                 (init_num, final_num, _) = self._get_num_of_rows(driver)
 
                 # Construct dict by creating company tickers
@@ -130,6 +101,7 @@ class TableScrapper:
                             0].text)
                     company_attr_dict[company_ticker] = {}
 
+                # Add company names after tickers
                 for row_index in range(final_num - init_num + 1):
                     com_tck = list(company_attr_dict.keys())[int(init_num + row_index - 1)]
                     attr = driver.find_elements(
@@ -142,10 +114,11 @@ class TableScrapper:
                     # Click the corresponding header
                     tab_name = list(map_of_headers[name].keys())[0]
                     WebDriverWait(driver, 10).until(
-                        ec.element_to_be_clickable((By.XPATH, f"//*[@id='columns_{tab_name}']/a"))) \
+                        ec.element_to_be_clickable((By.XPATH,
+                                                    f"//*[@id='columns_{tab_name}']/a"))) \
                         .click()
-                    self.logger.info(f"Clicked on {tab_name}...")
 
+                    # Retrive the required information after clicking on the corresponding header
                     for row_index in range(final_num - init_num + 1):
                         column_index = list(map_of_headers[name].values())[0]-1
                         com_tck = list(company_attr_dict.keys())[
@@ -158,13 +131,12 @@ class TableScrapper:
                         company_attr_dict[com_tck][name] = attr
 
                 pbar.update(20)
-                CNT += 1
-                # break
                 WebDriverWait(driver, 2).until(ec.element_to_be_clickable(
                     (By.XPATH, "/html/body/div[1]/div[4]/div[2]/div/div/div/div/div[10]/div/"
                                "div[4]/div"))).click()
 
         self.logger.info("SCRAPPING IS DONE!!!")
+        self.logger.info(f"SCRAPPED DATA: {self.search_params} ")
         return company_attr_dict
 
 
@@ -178,14 +150,6 @@ def main():
     """
     scrapper = TableScrapper()
     scrapped_data = scrapper.scrap_the_table()
-    import csv
-
-    my_dict = scrapped_data
-
-    with open('try_selective_scrap.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
-        w = csv.DictWriter(f, my_dict.keys())
-        w.writeheader()
-        w.writerow(my_dict)
     return scrapped_data
 
 
