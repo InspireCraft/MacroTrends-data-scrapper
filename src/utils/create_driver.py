73a19b780b710a4b185ce35778e89c64f2505b7a
unknown <utku.tahan.ee@gmail.com>
2023-11-27 13:26:01 +0100

    adjust directory issues

--- src/utils/create_driver.py
+++ src/utils/create_driver.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from src.utils.Logger import Logger
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import json
+import os
 
-
-def create_driver(url, logger) -> "webdriver.chrome":
+def create_driver(url, logger_str="info") -> "webdriver.chrome":
     """Create driver object.
 
     Driver is an WebDriver object that interacts with the website. Clicking,
@@ -20,11 +23,34 @@ def create_driver(url, logger) -> "webdriver.chrome":
     driver : WebDriver object
 
     """
+    logger = Logger("create_driver", logger_str)
     logger.info("WebDriver is being created...")
-    options = Options()
-    options.add_argument("--headless")  # Run selenium without opening an actual browser
 
-    driver = webdriver.Chrome(options=options)  # Initialize the driver instance
+    os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    f_search = open("..\\src\\utils\\webdriverOptions.json")
+    options_dict = json.load(f_search)
+    f_search.close()
+
+    # Get desired webdriver options
+    opts = [opt for opt in options_dict["options"]]
+    logger.info(f"Webdriver Options = {opts}...")
+
+    # Add driver options to the driver
+    options = Options()
+    for opt in opts:
+        options.add_argument(opt)
+
+    driver = webdriver.Chrome(options=options)
+
+    # Finalize driver creation by calling the url
     driver.get(url)
     logger.info("WebDriver is created!!!")
     return driver
+
+def main():
+    return create_driver(url="https://www.macrotrends.net/stocks/stock-screener")
+
+if __name__ == "__main__":
+    driver = main()
+    driver.close()
+    driver.quit()
