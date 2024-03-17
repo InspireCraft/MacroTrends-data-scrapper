de272d304d30002313665071528ebd692d2a2e97
AlpSari <50492914+AlpSari@users.noreply.github.com>
2024-03-17 23:29:25 +0100

    src: refactor save_to_csv

--- src/scrap_the_table.py
+++ src/scrap_the_table.py
@@ -1,7 +1,8 @@
 # Import libraries
+from copy import deepcopy
 import csv
 import os
-from copy import deepcopy
+from typing import Any
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
@@ -111,8 +112,19 @@ class TableScrapper:
         return current_initial_number, current_final_number, number_of_rows_in_the_list
 
     @staticmethod
-    def save_to_csv(scrapped_data: dict, name_of_csv: str, path=None):
+    def save_to_csv(
+        scrapped_data: dict[str:dict[str:Any]],
+        name_of_csv: str,
+        ticker_column_str: str = None
+    ):
         """Save scrapped data to a csv file.
+
+        Scrapped data is a dictionary where scrapped parameters of a company is
+        stored by using their ticker as an identifer key. CSV file is expected
+        to keep the tickers in a column whose name is provided to the function.
+        Then, if a company ticker already exist in the provided csv file, the
+        corresponding row will be updated. Otherwise, a new row will be appended
+        to the end of the file.
 
         Parameters
         ----------
@@ -122,97 +134,167 @@ class TableScrapper:
         name_of_csv : str
             name of the csv file that is wanted to be created
 
-        path : str
-            path of the CSV file where it is intended to be saved
+        ticker_column_str : str
+            column header name where the ticker values are stored in the csv file
         """
-        ########################################################################################
-        def overwrite_specific_row(csv_file, row_index, new_data, new_field_names):
-            temp_file = csv_file + ".temp"
-            # Write modified rows to temporary file
-            with open(csv_file, 'r', newline='') as infile, open(temp_file, 'w', newline='') as outfile:
-                reader = csv.reader(infile)
-                writer = csv.DictWriter(outfile, fieldnames=new_field_names)
-                writer.writeheader()
-                for i, row in enumerate(reader):
-                    print(f"i = {i}, row = {row}")
-                    if i == 0:
-                        existing_column_names = deepcopy(row)
-                        continue
-                    row_dict = {key: val for key, val in zip(existing_column_names, row)}
-                    if i == row_index+1:
-                        print(f"i = {i}")
-                        print(row_dict)
-                        print(new_data)
-                        row_dict.update(new_data)  # update row
-                    writer.writerow(row_dict)
+        if ticker_column_str is None:
+            ticker_column_str = "Ticker"
 
-            # Replace original file with temporary file
-            import os
-            os.replace(temp_file, csv_file)
+        # Extract column names from the dictionaries contained in . These values are the
+        # scrapped parameters, which we will be writing to the csv file. Each
+        # parameter will be in a separate column.
+        column_names_set = {
+            ticker_str for company_info_scrapped in scrapped_data.values()
+            for ticker_str in company_info_scrapped.keys()
+        }  # nested set comprehension (first for: outer)
 
-        #############################################################################################
+        # Make ticker column(ticker_column_str) and the company name ("name")
+        # first and the second column of the CSV file.
+        column_names_input_data = list(column_names_set)
+        column_names_input_data = TableScrapper._put_as_first_element(column_names_input_data, "name")
+        column_names_input_data.insert(0, ticker_column_str)
 
-
-        # Extract column names from the inner dictionaries
-        column_names = set()
-        for value in scrapped_data.values():
-            column_names.update(value.keys())
-
-        # Make "Name" as the first column name of the CSV
-        column_names = list(column_names)
-        try:
-            idx = column_names.index("name")
-            column_names.insert(0, column_names.pop(idx))
-        except ValueError:
-            pass
-
-        # Write to CSV
-        csv_file_name = name_of_csv+".csv"
-        ticker_column = "Ticker"
-        column_names.insert(0, ticker_column)
+        # Write data to CSV
+        csv_file_name = name_of_csv if name_of_csv.endswith(".csv") else name_of_csv + ".csv"
         if not os.path.exists(csv_file_name):
-            existing_headers = []
+            # CSV file does not exist, create one. Create the columns.
+            existing_column_names = []
             existing_tickers = []
             with open(csv_file_name, "w", newline="") as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=column_names)
+                writer = csv.DictWriter(csvfile, fieldnames=column_names_input_data)
                 writer.writeheader()
         else:
+            # CSV file exists, Read the columns and the company tickers to note
+            # which companies already have rows assigned to them in the CSV
+            # file.
             with open(csv_file_name, "r") as csvfile:
                 reader = csv.reader(csvfile)
-                existing_headers = next(reader)  # read first row
-                existing_tickers = []
-                for row in reader:
-                    existing_tickers.append(row[0])
+                existing_column_names = next(reader)  # read first row to get the headers.
 
-        existing_tick_dict = {key:value for key,value in scrapped_data.items() if key in existing_tickers}
-        nonexisting_tick_dict = {key:value for key,value in scrapped_data.items() if key not in existing_tickers}
-        print(f"exis: {len(list(existing_tick_dict.keys()))}, non: {len(list(nonexisting_tick_dict.keys()))}")
+                # Ticker column must already exist, assert that and get the column index
+                assert ticker_column_str in existing_column_names, \
+                    f"Ticker column provided (={ticker_column_str}) " \
+                    "does not exist in the first row of the existing CSV file. " \
+                    f"Headers in the file are: ({existing_column_names})"
 
-        # for key in list(scrapped_data.keys()):
-        #     if key in existing_headers:
-        #         existing_tick_dict[key] = []
-        #     else:
-        #         nonexisting_tick_dict[key] = []
-        #
+                try:
+                    # Search for the column where the ticker is stored
+                    ticker_column_index_in_file = existing_column_names.index(ticker_column_str)
+                except ValueError:
+                    # We put ticker first if it is up to us, so this is hard-coded for now
+                    ticker_column_index_in_file = 0
 
-        new_field_names = merge_unique_with_order(existing_headers, column_names)
-        ### UPDATE EXISTING TICKER ###
-        for ticker,value in existing_tick_dict.items():
-            row_index = existing_tickers.index(ticker)
-            overwrite_specific_row(csv_file_name, row_index, value, new_field_names)
+                # get all the existing tickers in the csv document
+                existing_tickers = [row[ticker_column_index_in_file] for row in reader]
 
+        # Stores all the columns names contained in the csv data and the current input
+        all_columns = merge_unique_with_order(existing_column_names, column_names_input_data)
 
+        # Split scrapped data into two subdictionaries, first corresponding to
+        # the data of the companies which already have a row in the csv file,
+        # second corresponding to the data of the companies which do not yet a
+        # row in the csv file.
+        data_for_existing_tickers = {}
+        data_for_new_tickers = {}
+
+        for ticker, company_data_scrapped in scrapped_data.items():
+            if ticker in existing_tickers:
+                data_for_existing_tickers[ticker] = company_data_scrapped
+            else:
+                data_for_new_tickers[ticker] = company_data_scrapped
+
+        # For the former part, corresponding row in the csv file will be
+        # updated, whereas for the latter part, a new row will be appended to
+        # the csv file with all the available information in the scrapped data.
+        # Update csv row using the data for existing tickers
+        TableScrapper._update_existing_rows(
+            csv_file_name,
+            ticker_column_str,
+            all_columns,
+            data_for_existing_tickers,
+        )
+
+        TableScrapper._append_rows(
+            csv_file_name,
+            ticker_column_str,
+            all_columns,
+            data_for_new_tickers,
+        )
+
+    @staticmethod
+    def _update_existing_rows(
+        csv_file,
+        ticker_column_str,
+        all_columns,
+        data_for_existing_tickers,
+    ):
+        """Overwrite rows of companies that already contain information in the csv file.
+
+        Args:
+        ----
+            csv_file (_type_): _description_
+            row_index (_type_): _description_
+            new_data (_type_): _description_
+            all_columns (_type_): _description_
+        """
+        temp_file = csv_file + ".temp"
+
+        # Write modified rows to temporary file
+        with open(csv_file, 'r', newline='') as infile, open(temp_file, 'w', newline='') as outfile:
+            reader = csv.reader(infile)
+            writer = csv.DictWriter(outfile, fieldnames=all_columns)
+            writer.writeheader()
+            for i, row in enumerate(reader):
+                if i == 0:
+                    # First row contains the headers
+                    existing_column_names = deepcopy(row)
+                    continue
+                # Second row and on (i.e., i >=1 ) contains company data
+                row_dict = {key: val for key, val in zip(existing_column_names, row)}
+                existing_ticker_str = row_dict[ticker_column_str]
+
+                if existing_ticker_str in data_for_existing_tickers:
+                    row_dict.update(data_for_existing_tickers[existing_ticker_str])  # update row
+                writer.writerow(row_dict)
+
+        # Replace original file with temporary file
+        import os
+        os.replace(temp_file, csv_file)
+
+    @staticmethod
+    def _append_rows(
+        csv_file_name,
+        ticker_column_str,
+        all_columns,
+        data_for_new_tickers: dict[str:Any]
+    ):
+        """Append rows to the csv for the companies which do not occupy a row in the data yet."""
         with open(csv_file_name, "a", newline="") as csvfile:
-
-            writer = csv.DictWriter(csvfile, fieldnames=new_field_names)
+            writer = csv.DictWriter(csvfile, fieldnames=all_columns)
 
             if csvfile.tell() == 0:
                 writer.writeheader()
 
-            for key, value in nonexisting_tick_dict.items():
-                row = {ticker_column: key}
+            for ticker, value in data_for_new_tickers.items():
+                row = {ticker_column_str: ticker}
                 row.update(value)
                 writer.writerow(row)
+
+    @staticmethod
+    def _put_as_first_element(input_list: list, value):
+        """Put the value in the list as the first element, if the value exist in the list.
+
+        Args:
+        ----
+            input_list (list): list that (possibly) contains the value
+            value (Any): value to be put as the first item in of the list
+        """
+        try:
+            idx = input_list.index(value)
+            input_list.insert(0, input_list.pop(idx))
+        except ValueError:
+            pass
+        return input_list
 
     def __del__(self):
         """Shut down the driver."""
