abda6a3149470f39e720ca9eb1aab860444545a5
unknown <utku.tahan.ee@gmail.com>
2024-03-17 17:09:54 +0100

    src: refactor scrap_the_table

--- src/scrap_the_table.py
+++ src/scrap_the_table.py
@@ -1,5 +1,6 @@
 # Import libraries
 import csv
+import os
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
@@ -43,6 +44,7 @@ class TableScrapper:
         self.driver_manager = DriverManager()  # Initialize driver manager object
         self.driver_manager.set_up_driver(url=url)  # Set up the driver by using the url
         self.logger = Logger(self.__class__.__name__, str_logger)
+        self.company_attr_dict = {}
 
     @staticmethod
     def _sort_search_parameters(params_to_be_searched):
@@ -86,7 +88,7 @@ class TableScrapper:
         return current_initial_number, current_final_number, number_of_rows_in_the_list
 
     @staticmethod
-    def save_to_csv(scrapped_data: dict, name_of_csv: str):
+    def save_to_csv(scrapped_data: dict, name_of_csv: str, path=None):
         """Save scrapped data to a csv file.
 
         Parameters
@@ -96,18 +98,40 @@ class TableScrapper:
 
         name_of_csv : str
             name of the csv file that is wanted to be created
+
+        path : str
+            path of the CSV file where it is intended to be saved
         """
         # Extract column names from the inner dictionaries
         column_names = set()
         for value in scrapped_data.values():
             column_names.update(value.keys())
 
+        # Make "Name" as the first column name of the CSV
+        column_names = list(column_names)
+        try:
+            idx = column_names.index("name")
+            column_names.insert(0, column_names.pop(idx))
+        except ValueError:
+            pass
+
         # Write to CSV
-        with open(name_of_csv + ".csv", "w", newline="") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=[''] + list(column_names))
-            writer.writeheader()
+        csv_file_name = name_of_csv+".csv"
+        ticker_column = "Ticker"
+        column_names.insert(0, ticker_column)
+        if not os.path.exists(csv_file_name):
+            with open(csv_file_name, "w", newline="") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=column_names)
+                writer.writeheader()
+
+        with open(csv_file_name, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=column_names)
+
+            if csvfile.tell() == 0:
+                writer.writeheader()
+
             for key, value in scrapped_data.items():
-                row = {'': key}
+                row = {ticker_column: key}
                 row.update(value)
                 writer.writerow(row)
 
@@ -115,42 +139,35 @@ class TableScrapper:
         """Shut down the driver."""
         self.driver_manager.kill_driver()
 
-    def _scrap_tickers(self, company_attr_dict, num_of_companies_on_page):
-        """Scrap the tickers of the companies on the page.
+    def _scrap_ticker_and_company_names(self):
+        """Scrap the tickers of the companies on the page."""
+        (init_num, final_num, _) = self._get_num_of_rows(
+            self.driver_manager.driver
+        )
+        num_of_companies_on_page = final_num - init_num + 1
 
-        Parameters
-        ----------
-        company_attr_dict :  dict
-            dictionary that holds the attribbutes per ticker
-
-        num_of_companies_on_page :  int
-            integer that states the number of companies at the current page
-        """
         ticker_list = []
-        # Pre-set the company tickers
+        name_list = []
         for row_index in range(num_of_companies_on_page):
             # Get company tickers
             company_ticker = (
                 self.driver_manager.driver.find_elements(
                     By.XPATH, f"// *[ @ id = 'row{row_index}jqxGrid'] / div[2] / div")[
                     0].text)
-            company_attr_dict[company_ticker] = {}
+            ticker_list.append(company_ticker)
 
             # Get company names
             company_name = self.driver_manager.driver.find_elements(
                 By.XPATH, f"//*[@id='row{row_index}jqxGrid']/"
                           f"div[1]/div/div/a")[0].text
-            company_attr_dict[company_ticker]['name'] = company_name
+            name_list.append(company_name)
 
-            # Store by-for-loop company tickers
-            ticker_list.append(company_ticker)
-        return company_attr_dict, ticker_list
+        # ticker_list, name_list = company_dict_temp.items()
+        return ticker_list, name_list
 
     def _fill_attribute_dict(
             self,
-            company_attr_dict,
             ticker_list,
-            num_of_companies_on_page,
             param,
             column_index
     ):
@@ -158,11 +175,17 @@ class TableScrapper:
 
         Parameters
         ----------
-        company_attr_dict : dict
+        company_attr_dict_temp : dict
         ticker_list : list[str]
         num_of_companies_on_page : int
         column_index : int
         """
+        (init_num, final_num, _) = self._get_num_of_rows(
+            self.driver_manager.driver
+        )
+        one_parameter_dict_per_page = {ticker: dict() for ticker in ticker_list}
+        num_of_companies_on_page = final_num - init_num + 1
+        # for ticker, row_index in zip(list(company_attr_dict_temp.keys()), range(num_of_companies_on_page)):
         for ticker, row_index in zip(ticker_list, range(num_of_companies_on_page)):
             # Get parameter values
             parameter_value = self.driver_manager.driver.find_elements(
@@ -170,27 +193,21 @@ class TableScrapper:
                 f"//*[@id='row{row_index}jqxGrid']/"
                 f"div[{int(3 + column_index)}]/div"
             )[0].text
-            company_attr_dict[ticker][param] = parameter_value
-        return company_attr_dict
+            one_parameter_dict_per_page[ticker][param] = parameter_value
+        return one_parameter_dict_per_page
 
-    def _scrap_the_page(self, company_attr_dict, scrap_params):
+    def _scrap_the_page(self, scrap_params):
         """Integrate filling the company dictionary methods.
 
         Parameters
         ----------
-        company_attr_dict : dict
         scrap_params : list
         """
-        (init_num, final_num, _) = self._get_num_of_rows(
-            self.driver_manager.driver
-        )
-        num_of_companies_on_page = final_num - init_num + 1
-
         # Scrap the tickers
-        company_attr_dict, ticker_list = self._scrap_tickers(
-            company_attr_dict,
-            num_of_companies_on_page
-        )
+        ticker_list, name_list = self._scrap_ticker_and_company_names()
+        company_attr_dict_page = {
+            ticker: {"name": name} for ticker, name in zip(ticker_list, name_list)
+        }
 
         # For each parameter to be scrapped, fill the dictionary
         previous_tab_name = None
@@ -200,57 +217,30 @@ class TableScrapper:
             column_index -= 1
 
             # Check if clicking onto a tab name is required
-            wait_time = 100
-            if previous_tab_name != tab_name:
-                WebDriverWait(self.driver_manager.driver, wait_time).until(
-                    ec.element_to_be_clickable(
-                        (By.XPATH, f"//*[@id='columns_{tab_name}']/a")
-                    )
-                ).click()
-
-                previous_tab_name = tab_name
+            previous_tab_name = self._change_tab(previous_tab_name, tab_name)
 
             # Fill dictionary ticker by ticker
-            company_attr_dict = self._fill_attribute_dict(
-                company_attr_dict,
+            one_parameter_dict_per_page = self._fill_attribute_dict(
                 ticker_list,
-                num_of_companies_on_page,
                 param,
                 column_index
             )
 
-        return company_attr_dict, final_num, num_of_companies_on_page
+            for key in list(one_parameter_dict_per_page.keys()):
+                company_attr_dict_page[key].update(one_parameter_dict_per_page[key])
 
-    def _integrated_scrap_module(self,
-                                 tqdm_length,
-                                 max_num,
-                                 final_num,
-                                 scrap_params,
-                                 company_attr_dict
-                                 ):
-        with tqdm(total=tqdm_length) as pbar:
-            while final_num != max_num:
-                # Scrap the current page
-                company_attr_dict, final_num, num_of_companies_on_page =  \
-                    self._scrap_the_page(
-                        company_attr_dict,
-                        scrap_params,
-                    )
+        return company_attr_dict_page
 
-                # Update the progress bar
-                pbar.update(int(num_of_companies_on_page))
+    def _change_tab(self, current_tab_name: str, tab_name: str):
+        wait_time = 100
+        if current_tab_name != tab_name:
+            WebDriverWait(self.driver_manager.driver, wait_time).until(
+                ec.element_to_be_clickable(
+                    (By.XPATH, f"//*[@id='columns_{tab_name}']/a")
+                )
+            ).click()
 
-                # Click on the clickable arrow on the table to progress in the pages
-                WebDriverWait(self.driver_manager.driver, 2).until(
-                    ec.element_to_be_clickable(
-                        (
-                            By.XPATH,
-                            "/html/body/div[1]/div[4]/div[2]/div/div/div/div/div[10]/div/div[4]/div"
-                        )
-                    )
-                ).click()
-
-        return company_attr_dict
+        return tab_name
 
     def scrap_the_table(self, parameters_to_be_scrapped=None):
         """Scrap the whole table including all tabs and pages in macro-trend.
@@ -282,65 +272,33 @@ class TableScrapper:
         # Get number of rows per page and total
         (init_num, final_num, max_num) = self._get_num_of_rows(self.driver_manager.driver)
 
-        # Initialize attribute dictionary
-        company_attr_dict = {}
+        tqdm_length = max_num - init_num
+        with tqdm(total=tqdm_length) as pbar:
+            while final_num != max_num:
+                # Scrap the current page
+                company_attr_current_page = self._scrap_the_page(scrap_params)
+                (init_num, final_num, _) = self._get_num_of_rows(self.driver_manager.driver)
+                # Update the progress bar
+                pbar.update(int(final_num - init_num + 1))
 
-        try:
-            company_attr_dict = self._integrated_scrap_module(
-                tqdm_length=max_num,
-                max_num=max_num,
-                final_num=final_num,
-                scrap_params=scrap_params,
-                company_attr_dict=company_attr_dict
+                # Click on the clickable arrow on the table to progress in the pages
+                self._progress_one_page()
 
-            )
-        except Exception:
-            self.logger.info("Exception occurred during scrapping")
-
-            # First of all save everything that is scrapped
-            name_of_the_csv_file = "scrapped_properties_before_error"
-            self.save_to_csv(company_attr_dict, name_of_the_csv_file)
-            self.logger.info(f"All materials upto exception is saved to {name_of_the_csv_file}.csv")
-
-            # Setup a new driver
-            self.driver_manager = DriverManager()  # Initialize driver manager object
-            self.driver_manager.set_up_driver(
-                url="https://www.macrotrends.net/stocks/stock-screener"
-            )
-
-            # Arrive at the page where scrapping stopped
-            self.logger.info("Driver arrive at the page where it left before exception")
-            num_of_companies_scrapped = len(list(company_attr_dict.keys()))
-            num_of_pages_scrapped = num_of_companies_scrapped // 20
-            for _ in range(num_of_pages_scrapped - 1):
-                WebDriverWait(self.driver_manager.driver, 2).until(
-                    ec.element_to_be_clickable(
-                        (
-                            By.XPATH,
-                            "/html/body/div[1]/div[4]/div[2]/div/div/div/div/div[10]/div/div[4]/div"
-                        )
-                    )
-                ).click()
-
-            self.logger.info("Scraping continues where it left:")
-            # Continue scrapping where it was left
-            (init_num, final_num, max_num) = self._get_num_of_rows(self.driver_manager.driver)
-            self.logger.info(f"FROM {init_num} -> {final_num}")
-
-            # Initiate scrapping
-            company_attr_dict = self._integrated_scrap_module(
-                tqdm_length=int(max_num - num_of_companies_scrapped),
-                max_num=max_num,
-                final_num=final_num,
-                scrap_params=scrap_params,
-                company_attr_dict=company_attr_dict
-
-            )
+                # Save the progress to the CSV file
+                self.save_to_csv(company_attr_current_page, "temp")
 
         self.logger.info("SCRAPPING IS DONE!!!")
         self.logger.info(f"SCRAPPED DATA: {scrap_params} ")
 
-        return company_attr_dict
+    def _progress_one_page(self):
+        WebDriverWait(self.driver_manager.driver, 2).until(
+            ec.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "/html/body/div[1]/div[4]/div[2]/div/div/div/div/div[10]/div/div[4]/div"
+                )
+            )
+        ).click()
 
 
 def main():
@@ -352,8 +310,7 @@ def main():
         Dictionary of the table in the given url
     """
     scrapper = TableScrapper()  # Initiate TableScrapper
-    scrapped_data = scrapper.scrap_the_table()
-    scrapper.save_to_csv(scrapped_data, "scrapped_properties")
+    scrapper.scrap_the_table()
 
 
 if __name__ == "__main__":
